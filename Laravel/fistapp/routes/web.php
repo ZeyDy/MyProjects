edf907358633c65f815d6c08f07b1c04b8ee3c6c
ZeyDy <zydrunasdauksas302pk@gmail.com>
2024-05-16 12:06:10 +0300

    Queues

--- Laravel/fistapp/routes/web.php
+++ Laravel/fistapp/routes/web.php
@@ -3,12 +3,20 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\LoginUserController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 
 Route::view('/', 'home');
 
 Route::view('/about', 'about');
+
+Route::get('test', function () {
+    $job = Job::first();
+    TranslateJob::dispatch($job);
+    return 'Done';
+});
 
 Route::get('/jobs', [JobController::class, 'index']);
 Route::get('/jobs/create', [JobController::class, 'create']);
